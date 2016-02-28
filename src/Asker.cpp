83d1ab7a6a6b0a4c8f41d2ac2b171495071fcf74
Eduardo Nascimento <egnascimento@gmail.com>
2016-02-28 19:39:27 -0300

    First commit

---
+++ src/Asker.cpp
@@ -0,0 +1,30 @@
+/*
+ * Asker.cpp
+ *
+ *  Created on: Feb 28, 2016
+ *      Author: eduardo
+ */
+
+#include "Asker.h"
+#include <iostream>
+
+using namespace std;
+
+
+
+
+Asker::Asker() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Asker::~Asker() {
+	// TODO Auto-generated destructor stub
+}
+
+Asker::Name() {
+	cout << "What is your name?" << endl;
+
+	return 0;
+
+}
---
+++ src/Asker.h
@@ -0,0 +1,20 @@
+/*
+ * Asker.h
+ *
+ *  Created on: Feb 28, 2016
+ *      Author: eduardo
+ */
+
+#ifndef ASKER_H_
+#define ASKER_H_
+
+
+class Asker {
+public:
+	Asker();
+	virtual ~Asker();
+
+	int Name();
+};
+
+#endif /* ASKER_H_ */
